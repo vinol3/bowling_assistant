92df3f8db3eeb6dd79ebdb1779257a12524d257a
flowwothchicken <oleksiivinichenko@gmail.com>
2025-12-26 15:18:33 +0100

    setup firestore to save throw data

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -83,4 +83,5 @@ class DefaultFirebaseOptions {
     authDomain: 'bowling-assistant.firebaseapp.com',
     storageBucket: 'bowling-assistant.firebasestorage.app',
   );
+
 }
--- lib/services/throw_service.dart
+++ lib/services/throw_service.dart
@@ -1,16 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import '../models/bowling_throw.dart';
 
 class ThrowService {
   static Future<void> saveThrow(BowlingThrow throwData) async {
+    debugPrint('DEBUG: saveThrow() called');
     final user = FirebaseAuth.instance.currentUser;
-    if (user == null) return;
+    if (user == null) {
+      debugPrint('DEBUG: No authenticated user, throw NOT saved');
+      return;
+    }
 
     await FirebaseFirestore.instance
         .collection('users')
         .doc(user.uid)
         .collection('throws')
         .add(throwData.toMap());
+    
+    debugPrint('DEBUG: Throw saved for user ${user.uid}');
   }
 }
