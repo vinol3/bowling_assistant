5e8b0199b706550bb75c949de13ac580cc49abcd
flowwothchicken <oleksiivinichenko@gmail.com>
2026-01-17 15:00:47 +0100

    removed unused scripts

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,9 @@ import 'auth/auth_gate.dart';
 import 'theme/app_theme.dart';
 import 'theme/theme_controller.dart';
 
-import 'screens/calibration_screen.dart';
 import 'screens/record_screen.dart';
-import 'screens/import_screen.dart';
 import 'screens/analysis_screen.dart';
 import 'screens/result_screen.dart';
-import 'screens/compare_screen.dart';
 import 'screens/settings_screen.dart';
 
 late List<CameraDescription> cameras;
@@ -60,23 +57,14 @@ class MainApp extends StatelessWidget {
             Widget page;
 
             switch (settings.name) {
-              case '/calibration':
-                page = const CalibrationScreen();
-                break;
               case '/record':
                 page = const RecordScreen();
-                break;
-              case '/import':
-                page = const ImportScreen();
                 break;
               case '/analysis':
                 page = const AnalysisScreen();
                 break;
               case '/results':
                 page = const ResultsScreen();
-                break;
-              case '/compare':
-                page = const CompareScreen();
                 break;
               case '/settings':
                 page = const SettingsScreen();
